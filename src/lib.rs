d93c7681e124723a829854f83d0f06491e78de0d
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2021-06-16 11:46:15 +0000

    up

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![feature(custom_test_frameworks)]
 #![feature(c_variadic)]
+extern crate no_std_compat as std;
 
 pub mod driver;
 
--- src/linux/printk.rs
+++ src/linux/printk.rs
@@ -1,6 +1,7 @@
-use cstr_core::c_char;
+use std::os::raw::c_char;
+use std::os::raw::c_int;
 
-#[no_mangle] pub unsafe extern "C" fn printk(_fmt : *const c_char, mut _args: ...) -> libc::c_int {
+#[no_mangle] pub unsafe extern "C" fn printk(_fmt : *const c_char, mut _args: ...) -> c_int {
     // WIP
     return 0;
 }
